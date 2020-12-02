8e801162147f328967d49769aaffe99a425e1872
anggifergian <anggyfergian56@gmail.com>
2020-12-02 20:32:31 +0700

    Add layout component and custom global css class

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <Link href='/'>
-      <a>Back to Home</a>
-    </Link>
+    <Layout>
+      <Head>
+        <title>Baka</title>
+      </Head>
+      <Link href='/'>
+        <a>Back to Home</a>
+      </Link>
+      <div className='custom'>First Post</div>
+    </Layout>
   );
 }
