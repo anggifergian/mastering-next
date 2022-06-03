bc1fccbd676d57ee09b8c22f1c8161fe31f63728
anggifergian <anggyfergian56@gmail.com>
2022-06-03 14:20:42 +0700

    Add on Scroll and create context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react'
+
+import { UIContext } from '../context/UIContext';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isScroll, setIsScroll] = useState(false)
+
+  const handleScroll = () => setIsScroll(window.scrollY >= 70)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <UIContext.Provider value={isScroll}>
+      <Component {...pageProps} />
+    </UIContext.Provider>
+  )
 }
 
 export default MyApp
